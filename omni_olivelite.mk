e7fa68f2093f73e24ea5fc59468e3024da4ba2dd
Milan Kragujević <milankragujevic@gmail.com>
2019-11-18 18:05:34 +0100

    v1

---
+++ omni_olivelite.mk
@@ -0,0 +1,27 @@
+$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+$(call inherit-product, vendor/omni/config/common.mk)
+
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    charger
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/kernel:kernel \
+	$(LOCAL_PATH)/recovery.fstab:recovery.fstab \
+	system/timezone/output_data/iana/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
+
+PRODUCT_DEVICE	:= olivelite
+PRODUCT_NAME	:= omni_olivelite
+PRODUCT_BRAND	:= Xiaomi
+PRODUCT_MODEL	:= Redmi (A
+PRODUCT_MANUFACTURER := Xiaomi
+
+BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := false
+
+PRODUCT_PROPERTY_OVERRIDES := ro.treble.enabled=true
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.build.version.security_patch=2099-12-31 \
+	ro.vendor.build.security_patch=2099-12-31
